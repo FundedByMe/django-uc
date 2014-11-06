c503d3cfea95acdc5469fe8e85ff11db3481cdde
Rebecca Meritz <rebecca@meritz.com>
2014-11-06 17:04:21 +0100

    Add FACTORY_FOR

--- uc/tests/factories.py
+++ uc/tests/factories.py
@@ -1,6 +1,10 @@
 import factory
 
+from ..models import UCRiskReport
+
 
 class UCReportFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = UCRiskReport
+
     rating = None
     last_updated = None
