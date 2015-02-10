29a4633df8ddcb5507f580e1936d85522c6dc4de
Flavia Missi <flaviamissi@gmail.com>
2015-02-10 12:56:05 -0200

    print xml's body before changing the product_code

--- uc/uc.py
+++ uc/uc.py
@@ -20,6 +20,7 @@ class VersionPlugin(MessagePlugin):
         company_report = body[0]
         company_report.set('ns1:product', self.product_code)
         company_report.set('ns1:version', '2.1')
+        print(context.envelope.getChild('Body'))
 
 
 def get_customer(client):
