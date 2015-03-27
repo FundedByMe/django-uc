8c9f13e710c3e3633a6dc656e848fea2e15efed0
Flavia Missi <flaviamissi@gmail.com>
2015-03-27 08:09:19 -0300

    stop asserting response and mock settings with fake values

--- uc/tests/test_uc.py
+++ uc/tests/test_uc.py
@@ -13,8 +13,8 @@ logging.getLogger('suds.resolver').setLevel(logging.INFO)
 request_body = """<ns0:Body>
    <ns1:companyReport ns1:product="4" ns1:version="2.1">
       <ns1:customer>
-         <ns1:userId>KOT92</ns1:userId>
-         <ns1:password>UT</ns1:password>
+         <ns1:userId>ll</ns1:userId>
+         <ns1:password>aa</ns1:password>
       </ns1:customer>
       <ns1:companyReportQuery xmlReply="true" htmlReply="false" reviewReply="false" lang="eng">
          <ns1:object>5561682518</ns1:object>
@@ -29,10 +29,8 @@ class UCTests(unittest.TestCase):
     @patch('__builtin__.print')
     def test_get_company_risk_report(self, print_mock):
         with patch("uc.uc.settings") as settings_mock:
-            settings_mock.UC_USER_ID = "KOT92"
-            settings_mock.UC_PASSWORD = "UT"
-            report = get_company_risk_report("5561682518")
+            settings_mock.UC_USER_ID = "ll"
+            settings_mock.UC_PASSWORD = "aa"
+            get_company_risk_report("5561682518")
             print_mock.assert_called_once_with(request_body)
-            rating = report.ucReport[0].xmlReply.reports[0].report[0].group[1].term[0].value
-            self.assertIsInstance(float(rating), float)
 
